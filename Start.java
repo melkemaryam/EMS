0d1880034ec05a85e258cdf4612565536a0fbb99
Hannah Claus <hannahmclaus@gmail.com>
2020-05-19 14:42:46 +0200

    added the start class to start the GUI

---
+++ Start.java
@@ -0,0 +1,7 @@
+public class Start {
+    // This is a main method that lets the program run, you can call this is any class, but should only be used once
+    public static void main(String[] args) {
+        // This is an object of the Login class
+        new LogInForm();
+    }
+}
