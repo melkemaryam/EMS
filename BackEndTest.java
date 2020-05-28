f92361332f7f139aca7d7f143c68c0dc23ba1474
worazz <57625531+worazz@users.noreply.github.com>
2020-05-28 21:02:01 +0100

    Add files via upload

--- BackEndTest.java
+++ BackEndTest.java
@@ -83,6 +83,61 @@ public class BackEndTest
         AccountManager.retrieveUser(test2);
         System.out.println("This results in field Role now being " + test2.getRole() + ".");
         System.out.println("That concluded AccountManager methods, moving on to EventManager. The key method of entire program is creating an Event");
+        System.out.println("For creation of events 10 values get put into DB and 3 will be set automatically - one value from Student object and two fields with default values");
+        String eventName = "Test event";
+        String description = "Test description";
+        int eventDay = 01;
+        int eventMonth = 01;
+        int eventYear = 01;
+        int eventHour = 01;
+        int eventMinute = 01;
+        String eventType = "physical";
+        int roomNo = 1;
+        int places = 60;
+        System.out.println("The values insterted into the database are as follows: " + eventName + " " + description + " " + eventDay + " " + eventMonth + " " + eventYear + " " + eventType + " " + roomNo + " " + places + ".");
+        EventManager.createEvent(eventName, description, eventType, eventDay, eventMonth, eventYear, eventHour, eventMinute, test2, roomNo, places);
+        System.out.println("Now to call on DB searching for events the method viewAllEvents() is called which should return Test event among any being in the DB at the time.");
+        ArrayList<String> eventList = EventManager.viewAllEvents();
+        StringBuilder se = new StringBuilder();
+            for (int i = eventList.size() - 1; i >=0; i--){
+                String event = eventList.get(i);
+                se.append(event);
+            }
+        System.out.println("The events found are:" + se);        
+        System.out.println("To get details of an event method searchEvents() is used, it will accept String eventname and return event object with data retrieved.");
+        Events specificEvent = EventManager.searchEvent(eventName);
+        System.out.println("Returned values are: " + specificEvent.getEventName() + " -name " + specificEvent.getDescription() + " -description " + specificEvent.getEventDay() + " " + specificEvent.getEventMonth() + " " + specificEvent.getEventYear() + " - date " + specificEvent.getEventHour() + " " + specificEvent.getEventMinute() + " - time " + specificEvent.getEventPlaces() + " - max participants " + specificEvent.getRoomNo() + " - room number " );
+        System.out.println("From that point user can edit the event using editEvent() method changing desciption and start time");
+        specificEvent.setDescription("A new testive-festive description");
+        specificEvent.setEventHour(22);
+        specificEvent.setEventMinute(22);
+        EventManager.editEvent(specificEvent);
+        System.out.println("Now a new Event object will be set up and read the updated values for Test Event");
+        Events testEvent = EventManager.searchEvent(eventName);
+        System.out.println("Returned values are: " + testEvent.getEventName() + " -name " + testEvent.getDescription() + " -description " + testEvent.getEventDay() + " " + testEvent.getEventMonth() + " " + testEvent.getEventYear() + " - date " + testEvent.getEventHour() + " " + testEvent.getEventMinute() + " - time " + testEvent.getEventPlaces() + " - max participants " + testEvent.getRoomNo() + " - room number " );          
+        System.out.println("Another method will show only Events which are created by logged in User ");
+        ArrayList<String> shortList = EventManager.viewOwnEvents(test2);
+        StringBuilder sl = new StringBuilder();
+            for (int i = shortList.size() - 1; i >=0; i--){
+                String event = shortList.get(i);
+                sl.append(event);
+            }
+        System.out.println("The events found are:" + sl);
+        System.out.println("Then there is cancelEvent() method which changes event visibility to 0, so I will not be searchable");
+        EventManager.cancelEvent(testEvent.getEventName());
+        System.out.println("Now to make sure the list will be called once again and should not show Test Event");
+        ArrayList<String> testList = EventManager.viewAllEvents();
+        StringBuilder sa = new StringBuilder();
+            for (int i = testList.size() - 1; i >=0; i--){
+                String event = testList.get(i);
+                sa.append(event);
+            }
+        System.out.println("The events found are:" + sa);  
+        System.out.println("Last Manager Class is BookingManager");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
         System.out.println("");
         System.out.println("");
         AccountManager.removeTestUser(test2);
